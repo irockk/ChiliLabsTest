7e58f0174eff30f5edbca1fbdd07b1237536f447
irinakosenkova <Lrinakos2002@gmail.com>
2025-03-18 13:07:06 +0200

    Add most used library dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,23 +38,52 @@ android {
     buildFeatures {
         compose = true
     }
+    android {
+        testOptions {
+            unitTests.all {
+                it.useJUnitPlatform()
+            }
+        }
+    }
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.kotlinDependencies.coroutines)
+
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.navigation)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
+
+    implementation(libs.koin.core)
+    implementation(libs.koin.annotations)
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+    implementation(libs.core.ktx)
+    testImplementation(libs.testng)
+    ksp(libs.koin.compiler)
+
+    implementation(libs.pagingDependencies.runtime)
+    implementation(libs.pagingDependencies.compose)
+
     androidTestImplementation(libs.androidx.junit)
+    testRuntimeOnly(libs.junit.platform)
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.engine)
+
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.mockitoDependency.core)
+    testImplementation(libs.mockitoDependency.kotlin)
+    testImplementation(libs.kotlinDependencies.coroutines.test)
+    testImplementation(libs.mockitoDependency.inline)
 }
